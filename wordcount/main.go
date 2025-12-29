3937075b3e38880c296e3890981d2a2e19eda1e4
rogovk <rogovkostyas@yandex.ru>
2025-12-30 00:12:41 +0300

    solutino of wordcount

--- wordcount/main.go
+++ wordcount/main.go
@@ -2,6 +2,55 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
 
+func main() {
+	wordcount := make(map[string]int)
+	// fmt.Println("Start working")
+	for _, file := range os.Args[1:] {
+		f, err := os.Open(file)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		// fmt.Println("File opened")
+		defer f.Close()
+		fileData, err := io.ReadAll(f)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		// fmt.Println("File data read")
+		for _, line := range strings.Split(string(fileData), "\n") {
+			// for _, word := range strings.Split(line, " ") {
+			// 	// if word == "" {
+			// 	// 	continue
+			// 	// }
+			// 	// fmt.Println("Word:", word)
+			// 	cnt, ok := wordcount[word]
+			// 	if !ok {
+			// 		wordcount[word] = 0
+			// 	}
+			// 	wordcount[word] = cnt + 1
+			// }
+			cnt, ok := wordcount[line]
+			if !ok {
+				wordcount[line] = 0
+			}
+			wordcount[line] = cnt + 1
+
+		}
+	}
+
+	for word, cnt := range wordcount {
+		if cnt >= 2 {
+			fmt.Printf("%d\t%s\n", cnt, word)
+		}
+	}
 }
