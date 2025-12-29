4d8ea4930dad66fbaed99f91c24404aec11ae50b
rogovk <rogovkostyas@yandex.ru>
2025-12-30 00:54:43 +0300

    solution of hotels

--- hotelbusiness/hotels.go
+++ hotelbusiness/hotels.go
@@ -2,6 +2,10 @@
 
 package hotelbusiness
 
+import (
+	"sort"
+)
+
 type Guest struct {
 	CheckInDate  int
 	CheckOutDate int
@@ -12,6 +16,43 @@ type Load struct {
 	GuestCount int
 }
 
+type Event struct {
+	IsComeIn bool
+	Date     int
+}
+
 func ComputeLoad(guests []Guest) []Load {
-	return []Load{}
+	events := []Event{}
+	for _, guest := range guests {
+		events = append(events, Event{IsComeIn: true, Date: guest.CheckInDate})
+		events = append(events, Event{IsComeIn: false, Date: guest.CheckOutDate})
+	}
+	sort.Slice(events, func(i, j int) bool {
+		return events[i].Date < events[j].Date
+	})
+	loads := []Load{}
+	if len(events) == 0 {
+		return loads
+	}
+
+	cur_date := events[0].Date
+	cur_cnt := 0
+	prev_cnt := -1
+	for _, event := range events {
+		if event.Date > cur_date {
+			if prev_cnt != cur_cnt {
+				loads = append(loads, Load{StartDate: cur_date, GuestCount: cur_cnt})
+				prev_cnt = cur_cnt
+			}
+			cur_date = event.Date
+		}
+		if event.IsComeIn {
+			cur_cnt += 1
+		} else {
+			cur_cnt -= 1
+		}
+	}
+	loads = append(loads, Load{StartDate: cur_date, GuestCount: cur_cnt})
+
+	return loads
 }
