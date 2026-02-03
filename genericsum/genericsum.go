611132b89f886e47d78c224971d3b689e69f88a9
Konstantin Rogov <rogov.ks@phystech.edu>
2026-02-03 22:47:16 +0300

    solution of genericsum and reflect/reversemap

--- genericsum/genericsum.go
+++ genericsum/genericsum.go
@@ -2,26 +2,110 @@
 
 package genericsum
 
-func Min(a, b int) int {
-	panic("implement me")
+import (
+	"math/cmplx"
+	"sort"
+	"sync"
+
+	"golang.org/x/exp/constraints"
+)
+
+func Min[T constraints.Ordered](x, y T) T {
+	if x < y {
+		return x
+	}
+	return y
 }
 
-func SortSlice(a []int) {
-	panic("implement me")
+func SortSlice[S ~[]T, T constraints.Ordered](s S) S {
+	sort.Slice(s, func(i, j int) bool {
+		return s[i] < s[j]
+	})
+	return s
 }
 
-func MapsEqual(a, b map[int]int) bool {
-	panic("implement me")
+func MapsEqual[K comparable, V comparable](a, b map[K]V) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for k, v := range a {
+		if b[k] != v {
+			return false
+		}
+	}
+	return true
 }
 
-func SliceContains(s []int, v int) bool {
-	panic("implement me")
+func SliceContains[S ~[]T, T comparable](s S, v T) bool {
+	for _, item := range s {
+		if item == v {
+			return true
+		}
+	}
+	return false
 }
 
-func MergeChans(chs ...<-chan int) <-chan int {
-	panic("implement me")
+func MergeChans[T any](chs ...<-chan T) <-chan T {
+	out := make(chan T)
+	var wg sync.WaitGroup
+	wg.Add(len(chs))
+
+	for _, ch := range chs {
+		go func(c <-chan T) {
+			defer wg.Done()
+			for val := range c {
+				out <- val
+			}
+		}(ch)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
 }
 
-func IsHermitianMatrix(m [][]int) bool {
-	panic("implement me")
+func IsHermitianMatrix[T constraints.Integer | constraints.Float | constraints.Unsigned | constraints.Complex](m [][]T) bool {
+	if len(m) == 0 {
+		return true
+	}
+
+	for i := range m {
+		if len(m[i]) != len(m) {
+			return false
+		}
+		for j := range m[i] {
+			// Check if we're dealing with complex numbers
+			switch any(m[i][j]).(type) {
+			case complex64, complex128:
+				// Convert both values to complex128 for comparison
+				var valI128, valJ128 complex128
+				switch v := any(m[i][j]).(type) {
+				case complex64:
+					valI128 = complex128(v)
+				case complex128:
+					valI128 = v
+				}
+				switch v := any(m[j][i]).(type) {
+				case complex64:
+					valJ128 = complex128(v)
+				case complex128:
+					valJ128 = v
+				default:
+					return false
+				}
+				if valI128 != cmplx.Conj(valJ128) {
+					return false
+				}
+			default:
+				// Handle real numeric types
+				if m[i][j] != m[j][i] {
+					return false
+				}
+			}
+		}
+	}
+	return true
 }
--- reversemap/reversemap.go
+++ reversemap/reversemap.go
@@ -2,6 +2,40 @@
 
 package reversemap
 
+import "reflect"
+
+var supportedKinds = map[reflect.Kind]bool{
+	reflect.Int:        true,
+	reflect.Uint:       true,
+	reflect.String:     true,
+	reflect.Complex64:  true,
+	reflect.Complex128: true,
+}
+
+func isSupportedKind(kind reflect.Kind) bool {
+	return supportedKinds[kind]
+}
+
 func ReverseMap(forward interface{}) interface{} {
-	panic("implement me")
+	if reflect.TypeOf(forward).Kind() != reflect.Map {
+		panic("forward is not a map")
+	}
+	keysType := reflect.TypeOf(forward).Key()
+	valuesType := reflect.TypeOf(forward).Elem()
+	if !isSupportedKind(keysType.Kind()) {
+		panic("keys type is not supported")
+	}
+	if !isSupportedKind(valuesType.Kind()) {
+		panic("values type is not supported")
+	}
+	forwardValue := reflect.ValueOf(forward)
+	backwardType := reflect.MapOf(valuesType, keysType)
+	backward := reflect.MakeMap(backwardType)
+
+	for iter := forwardValue.MapRange(); iter.Next(); {
+		key := iter.Key()
+		value := iter.Value()
+		backward.SetMapIndex(value, key)
+	}
+	return backward.Interface()
 }
